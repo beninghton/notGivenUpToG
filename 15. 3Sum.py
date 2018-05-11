e4289549e73d7a4fb809f6cd238e399379a4913d
anton.bektiashkin <anton.bektiashkin@clearscale.net>
2018-05-11 20:31:02 +0700

    7

--- 15. 3Sum.py
+++ 15. 3Sum.py
@@ -34,8 +34,17 @@ class Solution:
 
 
     def threeSumOpt(self, nums):
+
+        if len(nums) < 3:
+            return []
+
+        if len(nums) == 3 and sum(nums) == 0:
+            return [nums]
+
+
         res = []
         nums.sort()
+
         for i in xrange(len(nums)-2):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
@@ -44,17 +53,28 @@ class Solution:
                 s = nums[i] + nums[l] + nums[r]
                 if s < 0:
                     l +=1
-                if s > 0:
+                elif s > 0:
                     r -= 1
                 else:
                     res.append([nums[i], nums[l], nums[r]])
-                    #while l < r and nums[l] == nums[l+1]:
-                    #    l += 1
-                    #while l < r and nums[r] == nums[r-1]:
-                    #    r -= 1
-                    #l += 1; r -= 1
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1; r -= 1
         return res
 
 sol = Solution()
 #print sol.threeSum([-3, 0, -47, 0, 50])
-print sol.threeSumOpt([-1, 0, 1, 2, -1, -4])
+print sol.threeSumOpt([-1,-1,-1,2])
+
+# Two shields from duplicates:
+#
+# 1. if i > 0 and nums[i] == nums[i-1]:  - Move i if repeats, move past repeats until i is unique
+#       continue
+#
+# 2. while l < r and nums[l] == nums[l+1]:      - Move lefts to right, until left value is unique
+#        l += 1
+#    while l < r and nums[r] == nums[r-1]:      - Move rights to left, until right value is unique
+#        r -= 1
+#
